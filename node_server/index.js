d38cdd96aab8d51cb3d9cd0863f9d54babd52cc1
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-02-15 20:08:12 +0330

    added badges --needs review

--- node_server/index.js
+++ node_server/index.js
@@ -12,11 +12,23 @@ async function initGame() {
   const schema = new Parse.Schema("Game");
   schema.addPointer("user1", "_User");
   schema.addPointer("user2", "_User");
-  schema.addString("fen", {
+  schema.addString("last_valid_fen", {
     required: true,
     defaultValue: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
   });
+  schema.addArray("user1_moves"); // in order
+  schema.addArray("user2_moves"); // in order
+  schema.addString("state"); //valid options: not_started, on_going, draw, user1_won, user2_won
   schema.save();
+  // 
+  var schemas = await Parse.Schema.all();
+  user_schema = schemas["_User"];
+  user_schema.addNumber("current_win_streak", {
+    required: true,
+    defaultValue: 0
+  });
+  user_schema.addArray("badges");
+  user_schema.save();
 }
 
 async function initDB() {
