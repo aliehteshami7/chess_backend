a279c9e4374f05797c4a13d93b17e487df1d07ac
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-02-19 17:13:55 +0330

    Format Fix

--- cloud/src/createGame.js
+++ cloud/src/createGame.js
@@ -1,13 +1,13 @@
-const Game = Parse.Object.extend("Game");
+const Game = Parse.Object.extend('Game');
 
 const hasActiveGame = async (user) => {
-  const u1Query = new Parse.Query("Game");
-  u1Query.equalTo("user1", user);
-  u1Query.containedIn("state", ["NOT_STARTED", "ON_GOING"]);
+  const u1Query = new Parse.Query('Game');
+  u1Query.equalTo('user1', user);
+  u1Query.containedIn('state', ['NOT_STARTED', 'ON_GOING']);
 
-  const u2Query = new Parse.Query("Game");
-  u2Query.equalTo("user2", user);
-  u2Query.containedIn("state", ["NOT_STARTED", "ON_GOING"]);
+  const u2Query = new Parse.Query('Game');
+  u2Query.equalTo('user2', user);
+  u2Query.containedIn('state', ['NOT_STARTED', 'ON_GOING']);
 
   const query = Parse.Query.or(u1Query, u2Query);
 
@@ -15,15 +15,15 @@ const hasActiveGame = async (user) => {
 };
 
 Parse.Cloud.define(
-  "createGame",
+  'createGame',
   async ({ user }) => {
     if (await hasActiveGame(user)) {
-      throw new Error("You have a active game!");
+      throw new Error('You have a active game!');
     }
     const game = new Game();
     return (await game.save({ user1: user }, { useMasterKey: true })).id;
   },
   {
     requireUser: true,
-  }
+  },
 );
