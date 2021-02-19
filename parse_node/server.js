2abd389bde0bd8685b1b0992fcbc591ee1343868
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-02-19 15:39:36 +0330

    Add files to make parse docke in local

---
+++ parse_node/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { ParseServer } from 'parse-server';
+import http from 'http';
+
+const app = express();
+const api = new ParseServer({
+  databaseURI: process.env.PARSE_SERVER_DATABASE_URI,
+  cloud: './cloud/main.cjs',
+  appId: process.env.PARSE_SERVER_APPLICATION_ID,
+  masterKey: process.env.PARSE_SERVER_MASTER_KEY,
+  javascriptKey: process.env.PARSE_SERVER_JAVASCRIPT_KEY,
+  liveQuery: { classNames: ['Game'] },
+});
+
+const port = 1337;
+const ws_port = 1338;
+
+// Serve the Parse API at /parse URL prefix
+app.use('/parse', api);
+
+app.listen(port, () => {
+  console.log(`parse-server running on port ${port}.`);
+});
+
+// initialize live query
+const httpServer = http.createServer(app);
+httpServer.listen(ws_port);
+const parseLiveQueryServer = ParseServer.createLiveQueryServer(httpServer);
