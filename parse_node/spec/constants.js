5effa299ab27185c9e483fa09aeca29e5f9938ba
Ali Ehteshami <ali.ehteshami7@gmail.com>
2021-02-20 00:35:27 +0330

    Add tests

---
+++ parse_node/spec/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+  APPLICATION_ID: "asdfEWFkej2l3kj2lfjasfjasdf9",
+  MASTER_KEY: "FHAKJdhieeifo34920fwkjhfafja",
+};
---
+++ parse_node/spec/utils/purge-parse-table.js
@@ -0,0 +1,31 @@
+/**
+ * Removes all rows from the Parse Database
+ * @param  {string} tablename the name of the parse table to be purged
+ * @return {Promise}  promise to destroy each item  in the table
+ */
+module.exports = function(Parse) {
+  return tablename => {
+    var tableQuery;
+    if (tablename === 'User') tableQuery = new Parse.Query(Parse.User);
+    else tableQuery = new Parse.Query(tablename);
+    return tableQuery.find({ useMasterKey: true }).then(items => {
+      var destroyQueue = [];
+      for (var i = 0; i < items.length; i++) {
+        destroyQueue.push(items[i].destroy({ useMasterKey: true }));
+      }
+      return Promise.all(destroyQueue).catch(e => {
+        console.log('Error destroying: ' + e.message);
+      });
+    });
+  };
+};
+
+fdescribe('A suite is just a function', function() {
+  var a;
+
+  it('and so is a spec', function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
