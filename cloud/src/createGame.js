23a1739416828d9f4d0cd3aa50193fd7555a3225
ab.reza <a.re.morteza@gmail.com>
2021-02-19 17:16:56 +0330

    some code styling

--- cloud/src/createGame.js
+++ cloud/src/createGame.js
@@ -1,29 +1,17 @@
-const Game = Parse.Object.extend("Game");
+import { hasActiveGame } from './hasActiveGame';
 
-const hasActiveGame = async (user) => {
-  const u1Query = new Parse.Query("Game");
-  u1Query.equalTo("user1", user);
-  u1Query.containedIn("state", ["NOT_STARTED", "ON_GOING"]);
-
-  const u2Query = new Parse.Query("Game");
-  u2Query.equalTo("user2", user);
-  u2Query.containedIn("state", ["NOT_STARTED", "ON_GOING"]);
-
-  const query = Parse.Query.or(u1Query, u2Query);
-
-  return (await query.count()) > 0;
-};
+const Game = Parse.Object.extend('Game');
 
 Parse.Cloud.define(
-  "createGame",
+  'createGame',
   async ({ user }) => {
     if (await hasActiveGame(user)) {
-      throw new Error("You have a active game!");
+      throw new Error('You have an active game!');
     }
     const game = new Game();
     return (await game.save({ user1: user }, { useMasterKey: true })).id;
   },
   {
     requireUser: true,
-  }
+  },
 );
